b671bd6e20433c31ec0f3f97f921422e6a48d0b7
Bob Wadholm <bobwadholm@gmail.com>
2019-07-25 14:06:23 -0500

    Fixed js error

--- docs/js/greybox.js
+++ docs/js/greybox.js
@@ -93,7 +93,8 @@ document.addEventListener("keydown", (e) => {
     const doc = editor.ownerDocument.defaultView
     let sel = doc.getSelection()
     let range = sel.getRangeAt(0)
-    const tabNode = document.createTextNode("\u00a0\u00a0")   range.insertNode(tabNode)
+    const tabNode = document.createTextNode("\u00a0\u00a0")
+    range.insertNode(tabNode)
     range.setStartAfter(tabNode)
     range.setEndAfter(tabNode)
     sel.removeAllRanges()
