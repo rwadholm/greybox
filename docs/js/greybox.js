2c5429df6889f12ac101c21b04ba2e9ade44f80c
Bob Wadholm <bobwadholm@gmail.com>
2019-07-25 13:57:47 -0500

    Added tabbing function

--- docs/js/greybox.js
+++ docs/js/greybox.js
@@ -85,6 +85,22 @@ document.addEventListener("keydown", (e) => {
       }
     });
   }
+
+  if (e.keyCode === 9) { // Tab key
+    e.preventDefault()
+    // Insert four non-breaking spaces for the tab key
+    var editor = e.target
+    var doc = editor.ownerDocument.defaultView
+    var sel = doc.getSelection()
+    var range = sel.getRangeAt(0)
+
+    var tabNode = document.createTextNode("\u00a0\u00a0");
+    range.insertNode(tabNode);
+    range.setStartAfter(tabNode);
+    range.setEndAfter(tabNode);
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
 }, false)
 
 if(ipcMain){
